5d1f07a2b191ad9399502dfeb006fee7bdc1ebe2
Yosra <122837360+yosrajarray@users.noreply.github.com>
2025-11-21 00:12:31 +0100

    Update index.js

--- src /index.js
+++ src /index.js
@@ -1,1 +1,20 @@
+// Import de Express
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Route principale
+app.get('/', (req, res) => {
+    res.send('Hello CI/CD Secure!');
+});
+
+// Nouvelle route pour test
+app.get('/status', (req, res) => {
+    res.json({ status: 'ok', timestamp: new Date() });
+});
+
+// Démarrage du serveur
+app.listen(port, () => {
+    console.log(`App running at http://localhost:${port}`);
+});
 
